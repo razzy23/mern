2ea7a3910296575f727cfb639944e3e780b4bccc
Dewmini-Vithanage <2020cs130@stu.ucsc.cmb.ac.lk>
2022-10-26 20:37:43 +0530

    added admin features

--- client/src/pages/nav.js
+++ client/src/pages/nav.js
@@ -10,7 +10,6 @@ function Logout() {
     alert("Logged out")
     localStorage.clear()
     window.location.href = '/'
-
 }
 
 
@@ -37,13 +36,15 @@ function Nav() {
                 flexDirection: 'row',
 
             }}>
-                {token1 && <ul>
-                <li style={liStyle}>
-                    <a href="/bookView">Books View</a>
-                </li>
-                <li style={liStyle}>
-                    <a href="/booksInsert">Insert</a>
-                </li></ul>}
+                <ul> {token1 &&
+                    <li style={liStyle}>
+                        <a href="/bookView">Books View</a>
+                    </li>}
+                    {token1.name=='admin' &&
+                    <li style={liStyle}>
+                        <a href="/booksInsert">Insert</a>
+                    </li>}
+                </ul>
             </ul>
 
 
--- server/models/user.model.js
+++ server/models/user.model.js
@@ -4,7 +4,7 @@ const Schema = mongoose.Schema;
 
 const User = new mongoose.Schema(
 	{
-		username: { type: String, required: true },
+		username: { type: String, required: true, unique: true},
 		email: { type: String, required: true, unique: true },
 		password: { type: String, required: true },
 		quote: { type: String },
