0d50541bcdb062d464173c223f8c7ce79abe3923
Nisura Indisa <nisurindisa21@gmail.com>
2022-10-26 10:04:10 +0530

    Added functionality

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { useState } from 'react'
+import axios from 'axios';
 import { BrowserRouter, Route, Routes, Link } from 'react-router-dom'
 import Login from './pages/login'
 import Register from './pages/register'
--- client/src/pages/bookview.js
+++ client/src/pages/bookview.js
@@ -1,68 +1,88 @@
-import { useState } from 'react'
-
+import { useState, useEffect } from 'react'
+import axios from 'axios';
+import {useNavigate} from 'react-router-dom'
 
 function App() {
+    const [data, setData] = useState({ books: [] })
 
-
-    async function booksview(event) {
-        event.preventDefault()
-        const response = await fetch('http://localhost:1337/books/', {
-            method: 'GET',
-
-        })
-        const data = await response.json()
-        const books = data.books
-        console.log(books)
-        console.log("jello")
-        if (data.user) {
-            localStorage.setItem('token', data.user)
-            alert("Login Successful")
+    useEffect(() => {
+        async function fetchData() {
+            const res = await axios.get('http://localhost:1337/books')
+                .then((response) => {
+                    setData(response.data);
+                });
         }
+        
+        fetchData()
+    }, []);
 
-        for(var i=0;i<books.length;i++){
-            document.getElementById('r').innerHTML = "<td id='title'></td><td id='author'></td><td id='genre'></td><td id='status'></td>"
-        }
-        for (var i = 0; i < books.length; i++) {
-            document.getElementById('title').innerHTML = books[i].title;
-            document.getElementById('author').innerHTML = books[i].author;
-            document.getElementById('genre').innerHTML = books[i].genre;
-            document.getElementById('status').innerHTML = books[i].status;
-        }
+    function Delete(id) {
+        const res = axios.delete('http://localhost:1337/books/'+id)
+            .then((response) => {
+                alert(response.data);
 
+            });
+            window.location.replace('/bookView');
+        
     }
+    console.log(data)
+
+    // async function booksview(event) {
+    //     event.preventDefault()
+    //     const response = await fetch('http://localhost:1337/books/', {
+    //         method: 'GET',
+
+    //     })
+    //     const data = await response.json()
+    //     const books = data.books
+    //     console.log(books)
+    //     console.log("jello")
+    //     if (data.user) {
+    //         localStorage.setItem('token', data.user)
+    //         alert("Login Successful")
+    //     }
+
+    //     for(var i=0;i<books.length;i++){
+    //         document.getElementById('r').innerHTML = "<td id='title'></td><td id='author'></td><td id='genre'></td><td id='status'></td>"
+    //     }
+    //     for (var i = 0; i < books.length; i++) {
+    //         document.getElementById('title').innerHTML = books[i].title;
+    //         document.getElementById('author').innerHTML = books[i].author;
+    //         document.getElementById('genre').innerHTML = books[i].genre;
+    //         document.getElementById('status').innerHTML = books[i].status;
+    //     }
+
+    // }
 
     return (
         <div style={{
             height: '50vh', display: 'flex', justifyContent: 'center', alignItems: 'center', flexDirection: 'column'
         }}>
-            <h1 onClick={booksview}>Books</h1><br/>
-            <table id='xtable' style={{width:"800px",textAlign:'center'}} >
-                <tr><th>Book title</th><th>Author</th><th>Genre</th><th>status</th></tr>
-                <tr>
-                    <td style={{textAlign:'left'}}>how to write commit messages</td>
-                    <td>nisura</td>
-                    <td >sci-fi</td>
-                    <td>available</td>
-                </tr>
-                <tr>
-                    <td style={{textAlign:'left'}}>how to eat water</td>
-                    <td>ravindu</td>
-                    <td >sci-fi</td>
-                    <td>available</td>
-                </tr>
-                <tr>
-                    <td style={{textAlign:'left'}}>how to row</td>
-                    <td>sahan</td>
-                    <td >fiction</td>
-                    <td>available</td>
-                </tr>
+            <table>
+                <tbody style={{ display: 'block', border: '1px solid black' }}>
+                    <tr style={{border:'1px solid black'}}>
+                        <th>ISBN</th>
+                        <th>Title</th>
+                        <th>Author</th>
+                        <th>Publisher</th>
+                    </tr>
+
+                    {data.books.map((book, index) => (
+                        <tr key={index}>
+                            <td>{book._id}</td>
+                            <td>{book.title}</td>
+                            <td>{book.author}</td>
+                            <td>{book.publisher}</td>
+                            <td><button onClick={()=>Delete(book._id)}>Delete</button></td>
+                        </tr>
+                    ))}
+                </tbody>
             </table>
-            {
-            
-        }
 
         </div>
-    );
+    )
 }
+
+
 
 export default App; 
